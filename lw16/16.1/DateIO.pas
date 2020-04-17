e7dcd3082ab4b40d016031cf5fa290ee22e0de71
ktodkf <62442562+ktodkf@users.noreply.github.com>
2020-04-17 17:45:08 +0300

    Update DateIO.pas

--- lw16/16.1/DateIO.pas
+++ lw16/16.1/DateIO.pas
@@ -25,18 +25,18 @@ IMPLEMENTATION
             THEN
               BEGIN      
                 READ (INPUT, Ch3); 
-                IF (Ch1 = 'J')AND (Ch2 = 'A') AND (Ch3 = 'N') THEN Mo := JAN ELSE
-                IF (Ch1 = 'F')AND (Ch2 = 'E') AND (Ch3 = 'B') THEN Mo := FEB ELSE
-                IF (Ch1 = 'M')AND (Ch2 = 'A') AND (Ch3 = 'R') THEN Mo := MAR ELSE
-                IF (Ch1 = 'A')AND (Ch2 = 'P') AND (Ch3 = 'R') THEN Mo := APR ELSE
-                IF (Ch1 = 'M')AND (Ch2 = 'A') AND (Ch3 = 'Y') THEN Mo := MAY ELSE
-                IF (Ch1 = 'J')AND (Ch2 = 'U') AND (Ch3 = 'N') THEN Mo := JUN ELSE
-                IF (Ch1 = 'J')AND (Ch2 = 'U') AND (Ch3 = 'L') THEN Mo := JUL ELSE
-                IF (Ch1 = 'A')AND (Ch2 = 'U') AND (Ch3 = 'G') THEN Mo := AUG ELSE
-                IF (Ch1 = 'S')AND (Ch2 = 'E') AND (Ch3 = 'P') THEN Mo := SEP ELSE
-                IF (Ch1 = 'O')AND (Ch2 = 'C') AND (Ch3 = 'T') THEN Mo := OCT ELSE
-                IF (Ch1 = 'N')AND (Ch2 = 'O') AND (Ch3 = 'V') THEN Mo := NOV ELSE
-                IF (Ch1 = 'D')AND (Ch2 = 'E') AND (Ch3 = 'C') THEN Mo := DEC
+                IF (Ch1 = 'J') AND (Ch2 = 'A') AND (Ch3 = 'N') THEN Mo := JAN ELSE
+                IF (Ch1 = 'F') AND (Ch2 = 'E') AND (Ch3 = 'B') THEN Mo := FEB ELSE
+                IF (Ch1 = 'M') AND (Ch2 = 'A') AND (Ch3 = 'R') THEN Mo := MAR ELSE
+                IF (Ch1 = 'A') AND (Ch2 = 'P') AND (Ch3 = 'R') THEN Mo := APR ELSE
+                IF (Ch1 = 'M') AND (Ch2 = 'A') AND (Ch3 = 'Y') THEN Mo := MAY ELSE
+                IF (Ch1 = 'J') AND (Ch2 = 'U') AND (Ch3 = 'N') THEN Mo := JUN ELSE
+                IF (Ch1 = 'J') AND (Ch2 = 'U') AND (Ch3 = 'L') THEN Mo := JUL ELSE
+                IF (Ch1 = 'A') AND (Ch2 = 'U') AND (Ch3 = 'G') THEN Mo := AUG ELSE
+                IF (Ch1 = 'S') AND (Ch2 = 'E') AND (Ch3 = 'P') THEN Mo := SEP ELSE
+                IF (Ch1 = 'O') AND (Ch2 = 'C') AND (Ch3 = 'T') THEN Mo := OCT ELSE
+                IF (Ch1 = 'N') AND (Ch2 = 'O') AND (Ch3 = 'V') THEN Mo := NOV ELSE
+                IF (Ch1 = 'D') AND (Ch2 = 'E') AND (Ch3 = 'C') THEN Mo := DEC
               END;
           END;
       END          
