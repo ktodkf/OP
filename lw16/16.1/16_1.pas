ba6516811833d9cef12129bdfc76839a9b486cd8
ktodkf <62442562+ktodkf@users.noreply.github.com>
2020-04-17 17:26:21 +0300

    Update 16_1.pas

--- lw16/16.1/16_1.pas
+++ lw16/16.1/16_1.pas
@@ -2,8 +2,8 @@ PROGRAM SortMonth(INPUT, OUTPUT);
 USES 
   DateIO;
 VAR
-  Mo1, Mo2 : Month;
-BEGIN{SortMonth}
+  Mo1, Mo2: Month;
+BEGIN {SortMonth}
   ReadMonth(INPUT, Mo1);
   ReadMonth(INPUT, Mo2);
   IF (Mo1 = NoMonth) OR (Mo2 = NoMonth)
