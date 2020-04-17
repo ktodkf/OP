e9f0f8aa1ce4aa44e9b1488077c9eb50902618a9
ktodkf <62442562+ktodkf@users.noreply.github.com>
2020-04-17 17:50:31 +0300

    Update 132.pas

--- 13.2/132.pas
+++ 13.2/132.pas
@@ -1,14 +1,14 @@
 PROGRAM Lexico1(INPUT, OUTPUT);
 VAR
   Result, Ch: CHAR;
-  F1, F2 : TEXT;
+  F1, F2: TEXT;
   
 PROCEDURE Lexico(VAR F1, F2: TEXT; VAR Result: CHAR);
-{Result 0, 1, 2 если лексикографический порядок F1 =, <, > чем F2       
-соответственно. Фактические параметры, соответствующие F1 и F2,
-должны быть различными} 
+{Result 0, 1, 2 РµСЃР»Рё Р»РµРєСЃРёРєРѕРіСЂР°С„РёС‡РµСЃРєРёР№ РїРѕСЂСЏРґРѕРє F1 =, <, > С‡РµРј F2       
+СЃРѕРѕС‚РІРµС‚СЃС‚РІРµРЅРЅРѕ. Р¤Р°РєС‚РёС‡РµСЃРєРёРµ РїР°СЂР°РјРµС‚СЂС‹, СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓСЋС‰РёРµ F1 Рё F2,
+РґРѕР»Р¶РЅС‹ Р±С‹С‚СЊ СЂР°Р·Р»РёС‡РЅС‹РјРё} 
 VAR
-  Ch1, Ch2 : CHAR;
+  Ch1, Ch2: CHAR;
 BEGIN {Lexico}
   RESET(F1);
   RESET(F2);
@@ -19,11 +19,11 @@ BEGIN {Lexico}
       READ(F1, Ch1);
       READ(F2, Ch2);
       IF (Ch1 < Ch2) OR (EOF(F1))
-      THEN {Ch1 < Ch2 или F1 короче F2}
+      THEN {Ch1 < Ch2 РёР»Рё F1 РєРѕСЂРѕС‡Рµ F2}
         Result := '1'
       ELSE
         IF (Ch1 > Ch2) OR (EOF(F2))
-        THEN {Ch1 > Ch2 или F2 короче F1}
+        THEN {Ch1 > Ch2 РёР»Рё F2 РєРѕСЂРѕС‡Рµ F1}
           Result := '2'
     END {WHILE}
 END; {Lexico}
@@ -35,7 +35,7 @@ BEGIN{Lexico1}
   DO
     BEGIN
       READ(INPUT, Ch);
-      WRITE( F1, Ch)
+      WRITE(F1, Ch)
     END;
     READLN(INPUT);
     WHILE NOT EOLN(INPUT)
@@ -46,7 +46,7 @@ BEGIN{Lexico1}
       END;
     READLN(INPUT);
     WRITELN(F1);
-    WRITELN(f2);
+    WRITELN(F2);
     Lexico(F1, F2, Result);
     WRITELN(Result)  
 END.{Lexico1}
