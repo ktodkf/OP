0a31291c493ffcef9974b14e78e7b62529f670f6
ktodkf <62442562+ktodkf@users.noreply.github.com>
2020-06-10 16:48:33 +0300

    Add files via upload

---
+++ lw19/191.pas
@@ -0,0 +1,29 @@
+PROGRAM Prime(INPUT, OUTPUT);
+CONST 
+  Min = 2;
+  Max = 100;
+VAR 
+  Sieve : SET OF Min..Max;
+  Next, Total : INTEGER;
+BEGIN{Prime}
+      Sieve := [Min..Max];
+      Next := Min;
+      Total := Min;
+      WRITELN('опнярше вхякю б дхюоюгнме дн ', Max, ': ');
+      WHILE Next <= Max
+      DO 
+        BEGIN
+          WHILE Total <= Max
+          DO 
+            BEGIN
+              Sieve := Sieve - [Total];
+              Total := Next + Total
+            END;
+          WRITE(Next, ' ');
+          WHILE (Next <= Max) AND (NOT(Next IN Sieve))
+          DO
+            Next := Next + 1;
+            Total := Next    
+        END;
+      WRITELN(OUTPUT)
+END.{Prime}     
