061343fce4853d5bb977e481b60dda38fee35916
ktodkf <62442562+ktodkf@users.noreply.github.com>
2020-06-21 13:48:10 +0300

    Update 182.pas

--- lw18/18.2/182.pas
+++ lw18/18.2/182.pas
@@ -11,6 +11,7 @@ VAR
   Ave, TotalScore, ClassTotal: INTEGER;
   Lastname : TEXT;
   Ch : CHAR;
+  Error: BOOLEAN; 
   
 PROCEDURE CopyLastname(VAR InpF, OutF: TEXT);
 VAR
@@ -23,11 +24,13 @@ BEGIN{CopyLastname}
       WRITE(OutF, Ch)
     END;
 END;{CopyLastname} 
+
 BEGIN {AverageScore}
   ClassTotal := 0;
+  Error := FALSE; 
   WRITELN('Student averages:');
   Student := 1;
-  WHILE Student <= ClassSize
+  WHILE (Student <= ClassSize) AND NOT(Error)
   DO 
     BEGIN
       TotalScore := 0;
@@ -35,36 +38,48 @@ BEGIN {AverageScore}
       REWRITE(Lastname);
       IF NOT(EOLN)
       THEN
-        CopyLastname(INPUT, Lastname) ;
+        CopyLastname(INPUT, Lastname);
       WRITELN(Lastname, ' ');
       RESET(Lastname);
-      WHILE (WhichScore <= NumberOfScores) AND NOT(EOLN)
+      WHILE (WhichScore <= NumberOfScores) AND NOT(Error)
       DO
         BEGIN
-          READ(NextScore);
-          IF (NextScore < 0) OR (NextScore > 100)
+          IF NOT(EOLN)
           THEN
-            WRITELN('Некорректные данные: ', NextScore)
-          ELSE
-            BEGIN   
-              TotalScore := TotalScore + NextScore;
-              WhichScore := WhichScore + 1 
-            END
+            BEGIN
+              READ(NextScore);
+              IF (NextScore < 0) OR (NextScore > 100)
+              THEN
+                Error := TRUE;
+            END  
+          ELSE   
+            Error := TRUE;  
+          TotalScore := TotalScore + NextScore;
+          WhichScore := WhichScore + 1 
         END;   
       READLN;
-      CopyLastname(Lastname, OUTPUT);
-      TotalScore := TotalScore * 10;
-      Ave := TotalScore DIV NumberOfScores;
-      IF Ave MOD 10 >= 5
+      IF (NOT Error)
       THEN
-        WRITE(Ave DIV 10 + 1)
-      ELSE
-        WRITE(Ave DIV 10);  
-      ClassTotal := ClassTotal + TotalScore;
+        BEGIN
+          CopyLastname(Lastname, OUTPUT);
+          TotalScore := TotalScore * 10;
+          Ave := TotalScore DIV NumberOfScores;
+          IF Ave MOD 10 >= 5
+          THEN
+            WRITE(Ave DIV 10 + 1)
+          ELSE
+            WRITE(Ave DIV 10);  
+          ClassTotal := ClassTotal + TotalScore;
+        END
     END;
   WRITELN;
-  WRITELN ('Class average:');
-  ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
-  WRITELN(ClassTotal DIV 10, '.',  ClassTotal MOD 10:1)
-END.  {AverageScore}
-
+  IF (NOT Error)
+  THEN
+    BEGIN
+      WRITELN ('Class average:');
+      ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
+      WRITELN(ClassTotal DIV 10, '.',  ClassTotal MOD 10:1)
+    END
+  ELSE
+    WRITELN('ERROR')    
+END.{AverageScore}
