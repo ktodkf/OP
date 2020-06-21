282b5dc42e0da3d50f21a31341539d58da314516
ktodkf <62442562+ktodkf@users.noreply.github.com>
2020-06-21 12:54:52 +0300

    Update 191.pas

--- lw19/191.pas
+++ lw19/191.pas
@@ -4,26 +4,26 @@ CONST
   Max = 100;
 VAR 
   Sieve : SET OF Min..Max;
-  Next, Total : INTEGER;
+  Next, Count : INTEGER;
 BEGIN{Prime}
-      Sieve := [Min..Max];
-      Next := Min;
-      Total := Min;
-      WRITELN('ПРОСТЫЕ ЧИСЛА В ДИАПАЗОНЕ ДО ', Max, ': ');
-      WHILE Next <= Max
-      DO 
-        BEGIN
-          WHILE Total <= Max
-          DO 
-            BEGIN
-              Sieve := Sieve - [Total];
-              Total := Next + Total
-            END;
-          WRITE(Next, ' ');
-          WHILE (Next <= Max) AND (NOT(Next IN Sieve))
-          DO
-            Next := Next + 1;
-            Total := Next    
-        END;
-      WRITELN(OUTPUT)
+  Sieve := [Min..Max];
+  Next := Min;
+  Count := Min;
+  WRITELN('ПРОСТЫЕ ЧИСЛА В ДИАПАЗОНЕ ДО ', Max, ': ');
+  WHILE Next <= Max
+    DO 
+      BEGIN
+        WHILE Count <= Max
+        DO 
+          BEGIN
+            Sieve := Sieve - [Count];
+            Count := Next + Count
+          END;
+        WRITE(Next, ' ');
+        WHILE (Next <= Max) AND (NOT(Next IN Sieve))
+        DO
+          Next := Next + 1;
+          Count := Next    
+      END;
+  WRITELN(OUTPUT)
 END.{Prime}     
